e9ab6f00673338719c1842a3143dbaf75e0c325a
R.Grouls <raoul.grouls@businessdecision.nl>
2022-04-25 12:47:35 +0200

    adding seed to build weights

--- src/models/build.py
+++ src/models/build.py
@@ -22,14 +22,15 @@ def init_network(
 
 
 def init_weights(
-    m: int, n: int, scale: float = 1e-1
+    m: int, n: int, scale: float = 1e-1, seed=42
 ) -> Tuple[torch.Tensor, torch.Tensor]:
+    torch.manual_seed(seed)
     return scale * torch.normal(0.0, scale, (m, n)), scale * torch.normal(
         0.0, scale, (n,)
     )
 
 
 def torch_network(
-    sizes: List[int], scale: float = 1e-1
+    sizes: List[int], scale: float = 1e-1, seed=42,
 ) -> List[Tuple[torch.Tensor, torch.Tensor]]:
-    return [init_weights(m, n, scale) for m, n in zip(sizes[:-1], sizes[1:])]
+    return [init_weights(m, n, scale, seed) for m, n in zip(sizes[:-1], sizes[1:])]
