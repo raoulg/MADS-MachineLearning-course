b3055f3d08a627543c323ca875233db812e04fee
R.Grouls <raoul.grouls@businessdecision.nl>
2022-04-25 14:42:51 +0200

    make NN gin configurable

--- src/models/imagemodels.py
+++ src/models/imagemodels.py
@@ -1,17 +1,19 @@
+import gin
 import torch
 from torch import nn
 
 
+@gin.configurable
 class NeuralNetwork(nn.Module):
-    def __init__(self, num_classes: int) -> None:
-        super(NeuralNetwork, self).__init__()
+    def __init__(self, num_classes: int, units1: int, units2: int) -> None:
+        super().__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28 * 28, 512),
+            nn.Linear(28 * 28, units1),
             nn.ReLU(),
-            nn.Linear(512, 512),
+            nn.Linear(units1, units2),
             nn.ReLU(),
-            nn.Linear(512, num_classes),
+            nn.Linear(units2, num_classes),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
