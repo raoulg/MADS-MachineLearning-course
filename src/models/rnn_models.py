d436ecd506bd7fd366236f383e7a6fe05f671d0a
R.Grouls <raoul.grouls@businessdecision.nl>
2022-05-16 15:47:39 +0200

    config

--- src/models/rnn_models.py
+++ src/models/rnn_models.py
@@ -50,7 +50,25 @@ class BaseRNN(nn.Module):
         return yhat
 
 
+class GRUmodel(nn.Module):
+    def __init__(
+        self, config: Dict, 
+    ) -> None:
+        super().__init__()
+        self.rnn = nn.GRU(
+            input_size=config["input_size"],
+            hidden_size=config["hidden_size"],
+            dropout=config["dropout"],
+            batch_first=True,
+            num_layers=config["num_layers"],
+        )
+        self.linear = nn.Linear(config["hidden_size"], config["output_size"])
 
+    def forward(self, x: Tensor) -> Tensor:
+        x, _ = self.rnn(x)
+        last_step = x[:, -1, :]
+        yhat = self.linear(last_step)
+        return yhat
 
 
 
