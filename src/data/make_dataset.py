b1a0bae7ecb75e26fa2374f9914bf0bd678f6ea3
R.Grouls <raoul.grouls@businessdecision.nl>
2022-05-16 16:17:42 +0200

    linting

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,15 +1,21 @@
+from __future__ import annotations
+
 from pathlib import Path
-from typing import Tuple
+from typing import List, Tuple
 
 import gin
 import numpy as np
 import pandas as pd
 import requests
 import tensorflow as tf
+import torch
 from loguru import logger
+from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+
+Tensor = torch.Tensor
 
 
 def build_grid(k: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
@@ -74,7 +80,46 @@ def get_sunspots(datadir: Path) -> pd.DataFrame:
     else:
         logger.info(f"{file} does not exist, retrieving")
         spots = requests.get("http://www.sidc.be/silso/INFO/snmtotcsv.php", stream=True)
-        spots = np.genfromtxt(spots.raw, delimiter=";")
-        data = pd.DataFrame(spots[:, 2:4], columns=["year", "MonthlyMean"])  # type: ignore # noqa: E501
+        spots_ = np.genfromtxt(spots.raw, delimiter=";")
+        data = pd.DataFrame(spots_[:, 2:4], columns=["year", "MonthlyMean"])  # type: ignore # noqa: E501
         data.to_csv(file, index=False)
     return data
+
+
+class Datagenerator:
+    def __init__(self, paths: List[Path], batchsize: int) -> None:
+        self.dataset = paths
+        self.size = len(self.dataset)
+        self.batchsize = batchsize
+
+    def __len__(self) -> int:
+        return int(len(self.dataset) / self.batchsize)
+
+    def __getitem__(self, idx: int) -> Tuple[Tensor, int]:
+        x_ = np.genfromtxt(self.dataset[idx])[:, 3:]
+        x = torch.tensor(x_).type(torch.float32)
+        y = self.dataset[idx].parent.name
+        return x, int(y) - 1
+
+    def __iter__(self) -> Datagenerator:
+        self.index = 0
+        self.index_list = torch.randperm(self.size)
+        return self
+
+    def __next__(self) -> Tuple[Tensor, Tensor]:
+        if self.index <= (len(self.dataset) - self.batchsize):
+            X = []  # noqa N806
+            Y = []  # noqa N806
+            for _ in range(self.batchsize):
+                x, y = self[int(self.index_list[self.index])]
+                X.append(x)
+                Y.append(y)
+                self.index += 1
+            X_ = pad_sequence(X, batch_first=True, padding_value=0)  # noqa N806
+            return X_, torch.tensor(Y)
+        else:
+            raise StopIteration
+
+
+def get_gestures(datadir: Path) -> None:
+    pass
--- src/models/metrics.py
+++ src/models/metrics.py
@@ -2,17 +2,16 @@ import torch
 from torch.utils.data import DataLoader
 
 Tensor = torch.Tensor
-from typing import Tuple
 
 
 class MASE:
-    def __init__(self, dataloader: DataLoader, horizon: int):
+    def __init__(self, dataloader: DataLoader, horizon: int) -> None:
         self.scale = self.naivenorm(dataloader, horizon)
 
     def __repr__(self) -> str:
         return f"MASE(scale={self.scale:.3f})"
 
-    def naivenorm(self, dataloader: DataLoader, horizon: int):
+    def naivenorm(self, dataloader: DataLoader, horizon: int) -> Tensor:
         elist = []
         for x, y in dataloader:
             yhat = self.naivepredict(x, horizon)
@@ -20,7 +19,7 @@ class MASE:
             elist.append(e)
         return torch.mean(torch.tensor(elist))
 
-    def naivepredict(self, x: Tensor, horizon: int) -> Tuple[Tensor, Tensor]:
+    def naivepredict(self, x: Tensor, horizon: int) -> Tensor:
         assert horizon > 0
         yhat = x[..., -horizon:, :].squeeze(-1)
         return yhat
