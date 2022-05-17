cf2f3a2658e62d428b3bdee3a346d75487062c82
R.Grouls <raoul.grouls@businessdecision.nl>
2022-05-17 10:28:15 +0200

    cleanup

--- src/models/rnn_models.py
+++ src/models/rnn_models.py
@@ -52,27 +52,7 @@ class BaseRNN(nn.Module):
         return yhat
 
 
-@gin.configurable
-class GRUmodel(nn.Module):
-    def __init__(
-        self,
-        config: Dict,
-    ) -> None:
-        super().__init__()
-        self.rnn = nn.GRU(
-            input_size=config["input_size"],
-            hidden_size=config["hidden_size"],
-            dropout=config["dropout"],
-            batch_first=True,
-            num_layers=config["num_layers"],
-        )
-        self.linear = nn.Linear(config["hidden_size"], config["output_size"])
 
-    def forward(self, x: Tensor) -> Tensor:
-        x, _ = self.rnn(x)
-        last_step = x[:, -1, :]
-        yhat = self.linear(last_step)
-        return yhat
 
 
 def trainbatches(
