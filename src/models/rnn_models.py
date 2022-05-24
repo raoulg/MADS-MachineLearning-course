6b2b6c084a0bfa62dd1f575ce1785fc0fd342ee9
R.Grouls <raoul.grouls@businessdecision.nl>
2022-05-24 11:56:19 +0200

    RNN models with embeddings and attention

--- src/models/rnn_models.py
+++ src/models/rnn_models.py
@@ -91,3 +91,57 @@ class AttentionGRU(nn.Module):
         last_step = x[:, -1, :]
         yhat = self.linear(last_step)
         return yhat
+
+
+class NLPmodel(nn.Module):
+    def __init__(
+        self,
+        config: Dict,
+    ) -> None:
+        super().__init__()
+        self.emb = nn.Embedding(config["vocab"], config["hidden_size"])
+        self.rnn = nn.GRU(
+            input_size=config["hidden_size"],
+            hidden_size=config["hidden_size"],
+            dropout=config["dropout"],
+            batch_first=True,
+            num_layers=config["num_layers"],
+        )
+        self.linear = nn.Linear(config["hidden_size"], config["output_size"])
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.emb(x)
+        x, _ = self.rnn(x)
+        last_step = x[:, -1, :]
+        yhat = self.linear(last_step)
+        return yhat
+
+class AttentionNLP(nn.Module):
+    def __init__(
+        self,
+        config: Dict,
+    ) -> None:
+        super().__init__()
+        self.emb = nn.Embedding(config["vocab"], config["hidden_size"])
+        self.rnn = nn.GRU(
+            input_size=config["hidden_size"],
+            hidden_size=config["hidden_size"],
+            dropout=config["dropout"],
+            batch_first=True,
+            num_layers=config["num_layers"],
+        )
+        self.attention = nn.MultiheadAttention(
+            embed_dim=config["hidden_size"],
+            num_heads=4,
+            dropout=config["dropout"],
+            batch_first=True,
+        )
+        self.linear = nn.Linear(config["hidden_size"], config["output_size"])
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.emb(x)
+        x, _ = self.rnn(x)
+        x, _ = self.attention(x.clone(), x.clone(), x)
+        last_step = x[:, -1, :]
+        yhat = self.linear(last_step)
+        return yhat
