f48ca71734ecfb79d2e6f314bedb1dd475ec0ed7
R.Grouls <raoul.grouls@businessdecision.nl>
2022-05-25 17:18:53 +0200

    summary function for trax

--- src/models/build.py
+++ src/models/build.py
@@ -3,6 +3,7 @@ from typing import List, Tuple
 import jax.numpy as jnp
 import torch
 from jax import random
+from trax.shapes import signature
 
 
 def random_layer_params(
@@ -36,3 +37,21 @@ def torch_network(
     seed: int = 42,
 ) -> List[Tuple[torch.Tensor, torch.Tensor]]:
     return [init_weights(m, n, scale, seed) for m, n in zip(sizes[:-1], sizes[1:])]
+
+def summary(model, X, init=1, counter=0):
+    output = X
+    input = signature(output)
+    if init == 1:
+        print(f'{"layer":<23} {"input":<19} {"dtype":^7}    {"output":<19} {"dtype":^7}')
+    for sub in model.sublayers:
+        name = str(sub.name)
+        if name == '':
+            continue
+        elif name == "Serial":
+            output = summary(sub, output, init+1, counter)
+        else:
+            output = sub.output_signature(input)
+            print(f'({counter}) {str(sub.name):<19} {str(input.shape):<19}({str(input.dtype):^7}) | {str(output.shape):<19}({str(output.dtype):^7})') 
+        input = output
+        counter += 1
+    return output
