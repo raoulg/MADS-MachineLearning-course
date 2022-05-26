c1d2ed0c3e244da150833873b1656f18e43a5825
R.Grouls <raoul.grouls@businessdecision.nl>
2022-05-26 13:27:42 +0200

    improved trax summary

--- src/models/build.py
+++ src/models/build.py
@@ -1,9 +1,13 @@
-from typing import List, Tuple
+from typing import List, Tuple, Union
 
 import jax.numpy as jnp
+import numpy.ndarray as ndarray
 import torch
 from jax import random
+from trax.layers.combinators import Serial as Traxmodel
 from trax.shapes import signature
+
+Array = Union[jnp.ndarray, ndarray]
 
 
 def random_layer_params(
@@ -38,20 +42,27 @@ def torch_network(
 ) -> List[Tuple[torch.Tensor, torch.Tensor]]:
     return [init_weights(m, n, scale, seed) for m, n in zip(sizes[:-1], sizes[1:])]
 
-def summary(model, X, init=1, counter=0):
-    output = X
+
+def summary(
+    model: Traxmodel, X: Array, init: int = 1, counter: int = 0  # noqa N803
+) -> Array:
+    output = X  # noqa N803
     input = signature(output)
     if init == 1:
-        print(f'{"layer":<23} {"input":<19} {"dtype":^7}    {"output":<19} {"dtype":^7}')
+        print(
+            f'{"layer":<23} {"input":<19} {"dtype":^7}    {"output":<19} {"dtype":^7}'
+        )
     for sub in model.sublayers:
         name = str(sub.name)
-        if name == '':
+        if name == "":
             continue
         elif name == "Serial":
-            output = summary(sub, output, init+1, counter)
+            output = summary(sub, output, init + 1, counter)
         else:
             output = sub.output_signature(input)
-            print(f'({counter}) {str(sub.name):<19} {str(input.shape):<19}({str(input.dtype):^7}) | {str(output.shape):<19}({str(output.dtype):^7})') 
+            print(
+                f"({counter}) {str(sub.name):<19} {str(input.shape):<19}({str(input.dtype):^7}) | {str(output.shape):<19}({str(output.dtype):^7})"  # noqa E501
+            )
         input = output
         counter += 1
     return output
