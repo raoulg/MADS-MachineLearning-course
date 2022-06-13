bdf3a76ba47d15ee3c2b0e6a5a6de035e4a3cfc7
R.Grouls <raoul.grouls@businessdecision.nl>
2022-06-13 13:06:21 +0200

    visualize grid

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,9 +1,19 @@
-from typing import Dict
+from typing import Dict, Tuple
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 import seaborn as sns
+import numpy as np
 
 
 def plot_timers(timer: Dict[str, float]) -> None:
     x = list(timer.keys())
     y = list(timer.values())
     sns.barplot(x=x, y=y)
+
+def plot_grid(img: np.ndarray, k: int=3, figsize: Tuple = (10,10)) -> None:
+    fig, axs = plt.subplots(k, k, figsize=figsize)
+    axs = axs.ravel()
+    for i in tqdm(range(k*k)):
+        axs[i].imshow(img[i], cmap='gray')
+        axs[i].axis("off")
