22ce745dee53d77b7c0a9438fa631db9a25db164
Raoul Grouls <raoul.grouls@businessdecision.nl>
2022-11-17 17:29:21 +0100

    check if data exists

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -66,12 +66,15 @@ def get_MNIST(  # noqa: N802
 
 def get_flowers(data_dir: Path) -> Path:
     dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"  # noqa: E501
-    image_folder = tf.keras.utils.get_file(
-        "flower_photos", origin=dataset_url, untar=True, cache_dir=data_dir
-    )
-
-    image_folder = Path(image_folder)
-    logger.info(f"Data is downloaded to {image_folder}.")
+    image_folder = Path(data_dir) / "datasets/flower_photos"
+    if not image_folder.exists():
+        image_folder = tf.keras.utils.get_file(
+            "flower_photos", origin=dataset_url, untar=True, cache_dir=data_dir
+        )
+        image_folder = Path(image_folder)
+        logger.info(f"Data is downloaded to {image_folder}.")
+    else:
+        logger.info(f"Dataset already exists at {image_folder}")
     return image_folder
 
 
