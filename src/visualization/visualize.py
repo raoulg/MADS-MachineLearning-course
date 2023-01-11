0374b8b4f8c246d403c1ebfb55233088cd4478cf
Ninke Dijkstra <ninke.dijkstra@businessdecision.nl>
2023-01-11 19:08:00 +0100

    image explainerrr

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -21,3 +21,25 @@ def plot_grid(img: np.ndarray, filepath: Path, k: int=3, figsize: Tuple = (10,10
         axs[i].axis("off")
     fig.savefig(filepath)
     logger.success(f"saved grid to {filepath}")
+
+# Function to plot images
+def plot_categories(images, class_names):
+  fig, axes = plt.subplots(1, 11, figsize=(16, 15))
+  axes = axes.flatten()
+  
+  # Plot an empty canvas
+  ax = axes[0]
+  dummy_array = np.array([[[0, 0, 0, 0]]], dtype='uint8')
+  ax.set_title("reference")
+  ax.set_axis_off()
+  ax.imshow(dummy_array, interpolation='nearest')
+
+  # Plot an image for every category
+  for k,v in images.items():
+    ax = axes[k+1]
+    ax.imshow(v, cmap=plt.cm.binary)
+    ax.set_title(f"{class_names[k]}")
+    ax.set_axis_off()
+
+  plt.tight_layout()
+  plt.show()
