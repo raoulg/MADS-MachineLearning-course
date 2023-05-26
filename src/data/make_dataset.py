662aa8beee5733e74073a7fa5d29fa2b0f15992c
R.Grouls <rgrouls@gmail.com>
2023-05-26 09:16:01 +0000

    cleanup

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -101,9 +101,6 @@ def get_imdb_data(cache_dir: str = ".") -> Tuple[List[Path], List[Path]]:
 
         data_tools.get_file(datapath, "aclImdb_v1.tar.gz", url=url, overwrite=False)
 
-        # _ = tf.keras.utils.get_file(
-        #     "aclImdb_v1.tar.gz", url, untar=True, cache_dir=cache_dir, cache_subdir=""
-        # )
     testdir = datapath / "test"
     traindir = datapath / "train"
     keep_subdirs_only(testdir)
