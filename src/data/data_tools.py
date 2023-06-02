ab890bbf291fd7cdd9898b225435ac691cb89725
R.Grouls <rgrouls@gmail.com>
2023-06-02 18:55:01 +0000

    [data_tools] implement datafactories

--- src/data/data_tools.py
+++ src/data/data_tools.py
@@ -1,126 +1,20 @@
 from __future__ import annotations
 
-import random
 import shutil
 import tarfile
 import zipfile
 from datetime import datetime
 from pathlib import Path
-from typing import Callable, Iterator, List, Optional, Sequence, Tuple, Union, Protocol
+from typing import Iterator, List, Optional, Tuple, Union
 
-import numpy as np
 import requests
 import torch
 from loguru import logger
-from PIL import Image
-from torch.nn.utils.rnn import pad_sequence
 from tqdm import tqdm
-from abc  import ABC, abstractmethod, abstractproperty
-from enum import Enum
-from pydantic import BaseModel, HttpUrl
-from src.settings import DatasetSettings, flowersdatasetsettings, FileTypes
-from torchvision import transforms
+
+from src.settings import FileTypes
 
 Tensor = torch.Tensor
-
-class DatasetType(Enum):
-    FLOWERS = 1
-
-
-class DatasetProtocol(Protocol):
-    def __len__(self) -> int:
-        ...
-
-    def __getitem__(self, idx: int) -> Tuple:
-        ...
-
-class ProcessingDatasetProtocol(DatasetProtocol):
-    def process_data(self) -> None:
-        ...
-
-class DatastreamerProtocol(Protocol):
-    def stream(self) -> Iterator:
-        ...
-
-class AbstractDatasetFactory(ABC):
-    @abstractproperty
-    def settings(self) -> DatasetSettings:
-        raise NotImplementedError
-
-    @abstractmethod
-    def download_data(self, *args, **kwargs) -> None:
-        raise NotImplementedError
-
-    @abstractmethod
-    def create_dataset(self, *args, **kwargs) -> Dict[str, ProcessingDatasetProtocol]:
-        raise NotImplementedError
-
-    @abstractmethod
-    def create_datastreamer(self, *args, **kwargs) -> Dict[str, DatastreamerProtocol]:
-        raise NotImplementedError
-
-
-
-class FlowersDatasetFactory(AbstractDatasetFactory):
-    def __init__(self):
-        self._settings = flowersdatasetsettings
-
-    @property
-    def settings(self) -> DatasetSettings:
-        return self._settings
-
-    def download_data(self):
-        url = self._settings.dataset_url
-        filename = self._settings.filename
-        datadir = self._settings.data_dir
-        self.image_folder = Path(datadir) / "flower_photos"
-        if not self.image_folder.exists():
-            get_file(datadir, filename, url=dataset_url, overwrite=False)
-            logger.info(f"Data is downloaded to {self.image_folder}.")
-        else:
-            logger.info(f"Dataset already exists at {self.image_folder}")
-
-    def create_dataset(self):
-        self.download_data()
-        formats = self._settings.formats
-        paths, class_names = iter_valid_paths(self.image_folder, formats=formats)
-        paths = [*paths]
-        random.shuffle([paths])
-        trainidx = int(len(paths) * self._settings.trainfrac)
-        train = paths[:trainidx]
-        valid = paths[trainidx:]
-        traindataset = ImgDataset(train, class_names, img_size=self._settings.img_size)
-        validdataset = ImgDataset(valid, class_names, img_size=self._settings.img_size)
-        return {"train" : traindataset, "valid" : validdataset}
-
-    def create_datastreamer(self, batchsize: int, transformations = None):
-        datasets = self.create_dataset()
-        traindataset = datasets["train"]
-        validdataset = datasets["valid"]
-        if transformations is None:
-            transformations = transforms.Compose(
-                [
-                    transforms.ToTensor(),
-                    transforms.RandomHorizontalFlip(p=0.5),
-                    transforms.RandomVerticalFlip(p=0.5),
-                    transforms.RandomAffine(10),
-                ]
-            )
-        def _preprocess(batch: List[Tuple]):
-            X, y = zip(*batch)
-            X = torch.stack([transformations(x.squeeze()) for x in X])
-            return X, torch.tensor(y)
-        trainstreamer = BaseDatastreamer(traindataset, batchsize=batchsize, preprocessor=_preprocess)
-        validstreamer = BaseDatastreamer(validdataset, batchsize=batchsize, preprocessor=_preprocess)
-        return {"trainstreamer" : trainstreamer, "validstreamer" : validstreamer}
-
-
-class DatasetFactoryProvider:
-    @staticmethod
-    def get_factory(dataset_type: DatasetType) -> AbstractDatasetFactory:
-        if dataset_type == DatasetType.FLOWERS:
-            return FlowersDatasetFactory()
-
 
 
 def walk_dir(path: Path) -> Iterator:
@@ -143,7 +37,9 @@ def walk_dir(path: Path) -> Iterator:
         yield p.resolve()
 
 
-def iter_valid_paths(path: Path, formats: List[FileTypes]) -> Tuple[Iterator, List[str]]:
+def iter_valid_paths(
+    path: Path, formats: List[FileTypes]
+) -> Tuple[Iterator, List[str]]:
     """
     Gets all paths in folders and subfolders
     strips the classnames assuming that the subfolders are the classnames
@@ -162,8 +58,8 @@ def iter_valid_paths(path: Path, formats: List[FileTypes]) -> Tuple[Iterator, Li
     # retrieves foldernames as classnames
     class_names = [subdir.name for subdir in path.iterdir() if subdir.is_dir()]
     # keeps only specified formats
-    formats = [f.value for f in formats]
-    paths = (path for path in walk if path.suffix in formats)
+    formats_ = [f.value for f in formats]
+    paths = (path for path in walk if path.suffix in formats_)
     return paths, class_names
 
 
@@ -181,7 +77,9 @@ def get_file(
     response = requests.get(url, stream=True)
     total_size_in_bytes = int(response.headers.get("content-length", 0))
     block_size = 2**10
-    progress_bar = tqdm(total=total_size_in_bytes, unit="iB", unit_scale=True)
+    progress_bar = tqdm(
+        total=total_size_in_bytes, unit="iB", unit_scale=True, colour="#1e4706"
+    )
     logger.info(f"Downloading {path}")
     with open(path, "wb") as file:
         for data in response.iter_content(block_size):
@@ -194,11 +92,16 @@ def get_file(
 
 
 def extract(path: Path) -> None:
-    if path.suffix in [".zip"]:
+    try:
+        file_type = FileTypes(path.suffix)
+    except ValueError:
+        logger.info(f"The suffix {path.suffix} is not a recognized file type.")
+
+    if file_type in [FileTypes.ZIP]:
         logger.info(f"Unzipping {path}")
         with zipfile.ZipFile(path, "r") as zip_ref:
             zip_ref.extractall(path.parent)
-    if path.suffix in [".tgz", ".tar.gz", ".gz"]:
+    if file_type in [FileTypes.TGZ, FileTypes.TAR, FileTypes.GZ]:
         logger.info(f"Unzipping {path}")
         with tarfile.open(path, "r:gz") as tar:
             tar.extractall(path=path.parent)
@@ -246,231 +149,7 @@ def dir_add_timestamp(log_dir: Optional[Path] = None) -> Path:
     return log_dir
 
 
-class BaseDataset:
-    """The main responsibility of the Dataset class is to load the data from disk
-    and to offer a __len__ method and a __getitem__ method
-    """
-
-    def __init__(self, paths: List[Path]) -> None:
-        self.paths = paths
-        random.shuffle(self.paths)
-        self.dataset: List = []
-        self.process_data()
-
-    def process_data(self) -> None:
-        raise NotImplementedError
-
-    def __len__(self) -> int:
-        return len(self.dataset)
-
-    def __getitem__(self, idx: int) -> Tuple:
-        return self.dataset[idx]
-
-
-class FacesDataset(BaseDataset):
-    def __init__(self, paths: List[Path]) -> None:
-        super().__init__(paths)
-
-    def process_data(self) -> None:
-        for path in self.paths:
-            img = self.load_image(path)
-            self.dataset.append((img, path.name))
-
-    def load_image(self, path: Path) -> Image.Image:
-        img = Image.open(path)
-        return img
-
-
-class ImgDataset(BaseDataset):
-    def __init__(
-        self, paths: List[Path], class_names: List[str], img_size: Tuple[int, int]
-    ) -> None:
-        self.img_size = img_size
-        self.class_names = class_names
-        super().__init__(paths)
-
-    def process_data(self) -> None:
-        for file in self.paths:
-            img = self.load_image(file, self.img_size)
-            x = np.reshape(img, (1,) + img.shape)
-            y = self.class_names.index(file.parent.name)
-            self.dataset.append((x, y))
-
-    def load_image(self, path: Path, image_size: Tuple[int, int]) -> np.ndarray:
-        # load file
-        img_ = Image.open(path).resize(image_size, Image.LANCZOS)
-        return np.asarray(img_)
-
-    def __repr__(self) -> str:
-        return f"ImgDataset (imgsize {self.img_size}, #classes {len(self.class_names)})"
-
-
-class TSDataset(BaseDataset):
-    """This assume a txt file with numeric data
-    Dropping the first columns is hardcoded
-    y label is name-1, because the names start with 1
-
-    Args:
-        BaseDataset (_type_): _description_
-    """
-
-    def process_data(self) -> None:
-        for file in tqdm(self.paths):
-            x_ = np.genfromtxt(file)[:, 3:]
-            x = torch.tensor(x_).type(torch.float32)
-            y = torch.tensor(int(file.parent.name) - 1)
-            self.dataset.append((x, y))
-
-
-class TensorDataset:
-    """The main responsibility of the Dataset class is to
-    offer a __len__ method and a __getitem__ method
-    """
-
-    def __init__(self, data: Tensor, targets: Tensor) -> None:
-        self.data = data
-        self.targets = targets
-        assert len(data) == len(targets)
-
-    def __len__(self) -> int:
-        return len(self.targets)
-
-    def __getitem__(self, idx: int) -> Tuple:
-        return self.data[idx], self.targets[idx]
-
-
-class TextDataset(BaseDataset):
-    """This assumes textual data, one line per file
-
-    Args:
-        BaseDataset (_type_): _description_
-    """
-
-    def process_data(self) -> None:
-        for file in tqdm(self.paths):
-            with open(file) as f:
-                x = f.readline()
-            y = file.parent.name
-            self.dataset.append((x, y))
-
-
-class BaseDataIterator:
-    """This iterator will consume all data and stop automatically.
-    The dataset should have a:
-        __len__ method
-        __getitem__ method
-
-    """
-
-    def __init__(self, dataset: BaseDataset, batchsize: int) -> None:
-        self.dataset = dataset
-        self.batchsize = batchsize
-
-    def __len__(self) -> int:
-        return int(len(self.dataset) / self.batchsize)
-
-    def __iter__(self) -> BaseDataIterator:
-        self.index = 0
-        self.index_list = torch.randperm(len(self.dataset))
-        return self
-
-    def batchloop(self) -> Tuple[List, List]:
-        X = []  # noqa N806
-        Y = []  # noqa N806
-        for _ in range(self.batchsize):
-            x, y = self.dataset[int(self.index_list[self.index])]
-            X.append(x)
-            Y.append(y)
-            self.index += 1
-        return X, Y
-
-    def __next__(self) -> Tuple[Tensor, Tensor]:
-        if self.index <= (len(self.dataset) - self.batchsize):
-            X, Y = self.batchloop()  # noqa N806
-            return torch.tensor(X), torch.tensor(Y)
-        else:
-            raise StopIteration
-
-
-class PaddedDatagenerator(BaseDataIterator):
-    """Iterator with additional padding of X
-
-    Args:
-        BaseDataIterator (_type_): _description_
-    """
-
-    def __init__(self, dataset: BaseDataset, batchsize: int) -> None:
-        super().__init__(dataset, batchsize)
-
-    def __next__(self) -> Tuple[Tensor, Tensor]:
-        if self.index <= (len(self.dataset) - self.batchsize):
-            X, Y = self.batchloop()  # noqa N806
-            X_ = pad_sequence(X, batch_first=True, padding_value=0)  # noqa N806
-            return X_, torch.tensor(Y)
-        else:
-            raise StopIteration
-
-
-class BaseDatastreamer:
-    """This datastreamer wil never stop
-    The dataset should have a:
-        __len__ method
-        __getitem__ method
-
-    """
-
-    def __init__(
-        self,
-        dataset: BaseDataset,
-        batchsize: int,
-        preprocessor: Optional[Callable] = None,
-    ) -> None:
-        self.dataset = dataset
-        self.batchsize = batchsize
-        self.preprocessor = preprocessor
-        self.size = len(self.dataset)
-        self.reset_index()
-
-    def __len__(self) -> int:
-        return int(len(self.dataset) / self.batchsize)
-
-    def __repr__(self) -> str:
-        return f"{self.dataset} (streamerlen {len(self)})"
-
-    def reset_index(self) -> None:
-        self.index_list = np.random.permutation(self.size)
-        self.index = 0
-
-    def batchloop(self) -> Sequence[Tuple]:
-        batch = []
-        for _ in range(self.batchsize):
-            x, y = self.dataset[int(self.index_list[self.index])]
-            batch.append((x, y))
-            self.index += 1
-        return batch
-
-    def stream(self) -> Iterator:
-        while True:
-            if self.index > (self.size - self.batchsize):
-                self.reset_index()
-            batch = self.batchloop()
-            if self.preprocessor is not None:
-                X, Y = self.preprocessor(batch)  # noqa N806
-            else:
-                X, Y = zip(*batch)  # noqa N806
-            yield X, Y
-
-
-class VAEstreamer(BaseDatastreamer):
-    def stream(self) -> Iterator:
-        while True:
-            if self.index > (self.size - self.batchsize):
-                self.reset_index()
-            batch = self.batchloop()
-            # we throw away the Y
-            X_, _ = zip(*batch)  # noqa N806
-            X = torch.stack(X_)  # noqa N806
-            # change the channel to channel-last
-            X = torch.moveaxis(X, 1, 3)  # noqa N806
-            # and yield X, X
-            yield X, X
+def keep_subdirs_only(path: Path) -> None:
+    files = [file for file in path.iterdir() if file.is_file()]
+    for file in files:
+        file.unlink()
