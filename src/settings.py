c434a394016aca5d394316a15c0c5f0f1c758536
R.Grouls <rgrouls@gmail.com>
2023-06-02 18:53:54 +0000

    [settings] reporttypes, all datasetsettings

--- src/settings.py
+++ src/settings.py
@@ -1,30 +1,58 @@
+from __future__ import annotations
+
+from enum import Enum
 from pathlib import Path
-from typing import Any, Dict, List, Optional, Union, Tuple, cast
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    Dict,
+    List,
+    Optional,
+    Tuple,
+    Union,
+    cast,
+)
 
 import gin
 from loguru import logger
 from pydantic import BaseModel, HttpUrl, root_validator
 from ray import tune
 
-from src.models.metrics import Metric
-from enum import Enum
+from src.models import tokenizer
+
+if TYPE_CHECKING:
+    from src.models.metrics import Metric
 
 SAMPLE_INT = tune.search.sample.Integer
 SAMPLE_FLOAT = tune.search.sample.Float
 
+
 class FileTypes(Enum):
     JPG = ".jpg"
     PNG = ".png"
+    TXT = ".txt"
+    ZIP = ".zip"
+    TGZ = ".tgz"
+    TAR = ".tar.gz"
+    GZ = ".gz"
+
+
+class ReportTypes(Enum):
+    GIN = 1
+    TENSORBOARD = 2
+    MLFLOW = 3
+    RAY = 4
 
 
 @gin.configurable
 class TrainerSettings(BaseModel):
     epochs: int
-    metrics: List[Metric]
+    metrics: List[Callable]
     logdir: Path
     train_steps: int
     valid_steps: int
-    tunewriter: List[str] = ["tensorboard"]
+    reporttypes: List[ReportTypes]
     optimizer_kwargs: Dict[str, Any] = {"lr": 1e-3, "weight_decay": 1e-5}
     scheduler_kwargs: Optional[Dict[str, Any]] = {"factor": 0.1, "patience": 10}
     earlystop_kwargs: Optional[Dict[str, Any]] = {
@@ -69,6 +97,12 @@ class BaseSearchSpace(BaseModel):
             )
         return values
 
+    def __str__(self) -> str:
+        return "\n".join(f"{k}: {v}" for k, v in self.__dict__.items())
+
+    def __repr__(self) -> str:
+        return "\n".join(f"{k}: {v}" for k, v in self.__dict__.items())
+
 
 # this is what ray will use to create configs
 class SearchSpace(BaseSearchSpace):
@@ -89,30 +123,93 @@ class BaseSettings(BaseModel):
             )
         return values
 
+    def __str__(self) -> str:
+        return "\n".join(f"{k}: {v}" for k, v in self.__dict__.items())
+
+    def __repr__(self) -> str:
+        return "\n".join(f"{k}: {v}" for k, v in self.__dict__.items())
+
 
 cwd = Path(__file__).parent
 cwd = (cwd / "../").resolve()
 
 
 class GeneralSettings(BaseSettings):
-    data_dir = cwd / "data/raw"
+    data_dir: Path = cwd / "data/raw"
+
 
 class DatasetSettings(GeneralSettings):
     dataset_url: HttpUrl
     filename: Path
+    name: str
+    formats: List[FileTypes]
+
 
 class ImgDatasetSettings(DatasetSettings):
-    formats: List[FileTypes]
     trainfrac: float
     img_size: Tuple[int, int]
 
-flowersdatasetsettings = ImgDatasetSettings(
-    formats = [FileTypes.JPG],
-    dataset_url = cast(HttpUrl, "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"),
-    filename = Path("flowers.tgz"),
-    trainfrac = 0.8,
-    img_size = (224, 224)
+
+class TextDatasetSettings(DatasetSettings):
+    maxvocab: int
+    maxtokens: int
+    clean_fn: Callable
+
+
+class WindowedDatasetSettings(DatasetSettings):
+    horizon: int
+    window_size: int
+
+
+sunspotsettings = WindowedDatasetSettings(
+    dataset_url=cast(HttpUrl, "https://www.sidc.be/SILSO/DATA/SN_m_tot_V2.0.txt"),
+    filename=Path("sunspots.txt"),
+    name="sunspots",
+    formats=[],
+    horizon=3,
+    window_size=26,
 )
+
+fashionmnistsettings = DatasetSettings(
+    dataset_url=cast(HttpUrl, "https://github.com/zalandoresearch/fashion-mnist"),
+    filename=Path(""),
+    name="fashion",
+    formats=[],
+)
+
+gesturesdatasetsettings = DatasetSettings(
+    dataset_url=cast(
+        HttpUrl, "https://github.com/raoulg/gestures/raw/main/gestures-dataset.zip"
+    ),
+    filename=Path("gestures.zip"),
+    name="gestures",
+    formats=[FileTypes.TXT],
+)
+
+flowersdatasetsettings = ImgDatasetSettings(
+    dataset_url=cast(
+        HttpUrl,
+        "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz",
+    ),
+    filename=Path("flowers.tgz"),
+    name="flowers",
+    formats=[FileTypes.JPG],
+    trainfrac=0.8,
+    img_size=(224, 224),
+)
+
+imdbdatasetsettings = TextDatasetSettings(
+    dataset_url=cast(
+        HttpUrl, "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
+    ),
+    filename=Path("aclImdb_v1.tar.gz"),
+    name="imdb",
+    formats=[FileTypes.TXT],
+    maxvocab=10000,
+    maxtokens=100,
+    clean_fn=tokenizer.clean,
+)
+
 
 class VAESettings(GeneralSettings):
     h1: int = 250
