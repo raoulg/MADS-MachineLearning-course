a2d6fdabf9dadda31a171e9189963482e75e2f33
Raoul Grouls <Raoul.Grouls@han.nl>
2023-10-20 11:52:49 +0200

    update explainable les 7

--- notebooks/7_whatsnext/explainable/02_image_explainer.py
+++ notebooks/7_whatsnext/explainable/02_image_explainer.py
@@ -1,29 +1,66 @@
 import torch
 from torchvision import datasets
-from torchvision.transforms import ToTensor,Compose
+from torchvision.transforms import ToTensor
 from torch.utils.data import DataLoader
-from pathlib import Path
 from loguru import logger
 import torch.optim as optim
+import torch.nn as nn
 import sys
+from pydantic import BaseModel
+from pathlib import Path
+from mltrainer import metrics, Trainer, TrainerSettings, ReportTypes
 
 
 logger.add("/tmp/explainer.log")
 logger.add("explainer.log")
 
+
+class NeuralNetworkExplainer(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.flatten = nn.Flatten()
+
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(1, 10, kernel_size=5),
+            nn.MaxPool2d(2),
+            nn.ReLU(),
+            nn.Conv2d(10, 20, kernel_size=5),
+            nn.Dropout(),
+            nn.MaxPool2d(2),
+            nn.ReLU(),
+        )
+
+        self.fc_layers = nn.Sequential(
+            nn.Linear(320, 50),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(50, 10),
+            nn.Softmax(dim=1),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv_layers(x)
+        x = self.flatten(x)
+        out = self.fc_layers(x)
+        return out
+
+
+class ImageExplainerSettings(BaseModel):
+    data_dir: Path = Path("data")
+    epochs: int = 10
+    modelname: Path = Path("trained_model.pt")
+    imgpath: Path = Path("img")
+    num_classes: int = 10
+
+
 if __name__ == "__main__":
-    from src.settings import ImageExplainerSettings
-    from src.models.imagemodels import NeuralNetworkExplainer
-    from src.models import metrics, train_model
-    from src.settings import TrainerSettings, ReportTypes
-
-
     logger.info("start image_explainer.py")
     presets = ImageExplainerSettings()
 
     logger.info(f"Importing {sys.argv[1]}")
 
-    if sys.argv[1] == 'mnist':
+    if sys.argv[1] == "mnist":
         dataset_train = datasets.MNIST(
             root=presets.data_dir,
             train=True,
@@ -51,6 +88,8 @@ if __name__ == "__main__":
             download=True,
             transform=ToTensor(),
         )
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    logger.info(f"Using {device} device")
 
     train_dataloader = DataLoader(dataset_train, batch_size=128, shuffle=True)
     test_dataloader = DataLoader(dataset_test, batch_size=128, shuffle=True)
@@ -60,17 +99,13 @@ if __name__ == "__main__":
     logger.info(
         f"Length trainset: {len(dataset_train)}, length testset: {len(dataset_test)}"
     )
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    logger.info(
-        f"Using {device} device")
 
-    model = NeuralNetworkExplainer().to(device)
-    logger.info(f"{model}")
+    model = NeuralNetworkExplainer()
 
     loss_fn = torch.nn.CrossEntropyLoss()
     accuracy = metrics.Accuracy()
 
-    log_dir = "../../../models/explainer"
+    log_dir = "models/explainer"
 
     logger.info(f"starting training for {presets.epochs} epochs")
 
@@ -81,17 +116,17 @@ if __name__ == "__main__":
         train_steps=50,
         valid_steps=50,
         reporttypes=[ReportTypes.TENSORBOARD],
-        optimizer_kwargs = {"lr" : 1e-2}
+        optimizer_kwargs={"lr": 1e-2},
     )
 
-    trainer = train_model.Trainer(
+    trainer = Trainer(
         model=model,
         settings=settings,
         loss_fn=loss_fn,
         optimizer=optim.Adam,
         traindataloader=train_dataloader,
         validdataloader=test_dataloader,
-        scheduler=optim.lr_scheduler.ReduceLROnPlateau
+        scheduler=optim.lr_scheduler.ReduceLROnPlateau,
     )
     trainer.loop()
 
